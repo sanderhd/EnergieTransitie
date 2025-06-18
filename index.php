ddd129983a35008c053fb6f4dc2325b90969f51a
Sander <fristislekker@proton.me>
2025-06-18 10:09:19 +0200

    Basis Structuur en begin aan navbar. Database connectie en begin van SQL gemaakt.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Energie Transitie</title>
+    <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php"><img src="images/logo.png"> <h1>Energie Transitie</h1></a>
+
+        <div class="nav-right">
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+        </div>
+    </nav>
+</body>
+</html>
